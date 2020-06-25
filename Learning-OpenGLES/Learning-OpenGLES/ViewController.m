499fd025e27f64cfa8395f1b424402fe89a5d43b
jiachen <951311987@qq.com>
2020-06-25 20:49:19 +0800

     添加【3-1】

--- Learning-OpenGLES/Learning-OpenGLES/ViewController.m
+++ Learning-OpenGLES/Learning-OpenGLES/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import "ViewController_CH2_1.h"
+#import "ViewController_CH3_1.h"
 
 @interface ViewController ()
 
@@ -21,14 +22,32 @@
     
     UIButton *btn1 = [[UIButton alloc] initWithFrame:CGRectMake(10, 74.f, 80, 40)];
     btn1.backgroundColor = [UIColor redColor];
+    [btn1 setTitle:@"Ch3" forState:UIControlStateNormal];
     [btn1 addTarget:self
              action:@selector(onShowChapter2)
    forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn1];
+    
+    UIButton *btn2 = [[UIButton alloc] initWithFrame:CGRectMake(100, 74.f, 80, 40)];
+    btn2.backgroundColor = [UIColor redColor];
+    [btn2 setTitle:@"Ch3" forState:UIControlStateNormal];
+    [btn2 addTarget:self
+             action:@selector(onShowChapter3)
+   forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn2];
 }
 
 - (void)onShowChapter2 {
     ViewController_CH2_1 *vc = [[ViewController_CH2_1 alloc] init];
+    vc.modalPresentationStyle = 0;
+    [self presentViewController:vc
+                       animated:YES
+                     completion:^{}];
+
+}
+
+- (void)onShowChapter3 {
+    ViewController_CH3_1 *vc = [[ViewController_CH3_1 alloc] init];
     vc.modalPresentationStyle = 0;
     [self presentViewController:vc
                        animated:YES
